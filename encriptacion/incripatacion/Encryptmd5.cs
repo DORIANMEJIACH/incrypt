f233c16d6898fa415a329fbda51ef393e4c5910c
DORIANMEJIACH <dorian.mejiachay@gmail.com>
2024-07-29 22:45:58 -0600

    Add files via upload

---
+++ encriptacion/incripatacion/Encryptmd5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace encripcion
+{
+    internal class Encryptmd5
+    {
+        public string Encrypt(string mensaje)
+        {
+            string hash = "codigo con c";
+            byte[] data = UTF8Encoding.UTF8.GetBytes(mensaje);
+            MD5 md5 = MD5.Create();
+            TripleDES tripledes = TripleDES.Create();
+            tripledes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+            tripledes.Mode = CipherMode.ECB;
+            tripledes.Padding = PaddingMode.PKCS7; // Añadir modo de relleno
+
+            ICryptoTransform transform = tripledes.CreateEncryptor();
+            byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+            return Convert.ToBase64String(result);
+        }
+
+        public string Decrypt(string mensajeen)
+        {
+            string hash = "codigo con c";
+            byte[] data = Convert.FromBase64String(mensajeen);
+            MD5 md5 = MD5.Create();
+            TripleDES tripledes = TripleDES.Create();
+            tripledes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+            tripledes.Mode = CipherMode.ECB;
+            tripledes.Padding = PaddingMode.PKCS7; // Añadir modo de relleno
+
+            ICryptoTransform transform = tripledes.CreateDecryptor();
+            byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+            return UTF8Encoding.UTF8.GetString(result);
+        }
+    }
+}
---
+++ encriptacion/incripatacion/Program.cs
@@ -0,0 +1,35 @@
+﻿using encripcion;
+using System.Text.Json;
+
+class Program
+{
+    public class UserInfo
+    {
+        public string Usuario { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+    static void Main(string[] args)
+    {
+        string jsonString = "{\"Usuario\":\"User02420\",\"Password\":\"Inc0melS@\",\"Token\":\"aJkL9xPzT72QsW3V\"}";
+
+        
+
+       
+        Encryptmd5 encriptador = new Encryptmd5();
+
+     /* string mensajeOriginal = "Hola123";
+        string mensajeEncriptado = encriptador.Encrypt(jsonString);
+        Console.WriteLine($"Mensaje Encriptado: {mensajeEncriptado}");*/
+     Console.WriteLine("ingresa el incryptado");
+        string mensajeEncriptado = Console.ReadLine();
+        string mensajeDesencriptado = encriptador.Decrypt(mensajeEncriptado);
+        Console.WriteLine($"Mensaje Desencriptado: {mensajeDesencriptado}");
+        var userInfo = JsonSerializer.Deserialize<UserInfo>(jsonString);
+        Console.WriteLine(userInfo);
+
+
+        Console.ReadKey();
+    }
+   
+}
